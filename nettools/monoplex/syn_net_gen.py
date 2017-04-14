8ff4aae50de9340be70de3fc2315eb2c4df1b815
filip141 <filip141@gmail.com>
2017-04-14 14:11:35 +0200

    docstrings added

--- nettools/monoplex/syn_net_gen.py
+++ nettools/monoplex/syn_net_gen.py
@@ -14,9 +14,19 @@ class Network(object):
         self.weighted = weighted
 
     def network_degrees(self):
+        """
+        Method calculate degree for each node by summing adjacency matrix.
+
+        :return: Numpy array with nodes degree.
+        """
         return np.sum(self.network, axis=1)
 
     def degree_distribution(self):
+        """
+        Method calculate degree distribution using numpy bincount.
+
+        :return: Degree distribution (Numpy array).
+        """
         count_degs = np.bincount(self.network_degrees().astype(np.int32))
         args = np.argsort(count_degs)[::-1]
         deg_dst = np.sort(count_degs)[::-1]
@@ -25,6 +35,9 @@ class Network(object):
         return deg_dst.astype(np.float64) / np.sum(deg_dst), args
 
     def plot_loglog(self):
+        """
+        Plot log log degree distribution using degree_distribution method.
+        """
         ax = plt.gca()
         deg_dst, x_axis = self.degree_distribution()
         ax.set_xlim([np.min(x_axis), np.max(x_axis)])
@@ -34,11 +47,17 @@ class Network(object):
         plt.show()
 
     def plot_degree_dist(self):
+        """
+        Plot degree distribution using degree_distribution method.
+        """
         deg_dst, x_axis = self.degree_distribution()
         plt.scatter(x_axis, deg_dst, marker='o', color='r')
         plt.show()
 
     def show(self):
+        """
+        Draw networkx graph.
+        """
         nx_graph = nx.from_numpy_matrix(self.network)
         nx.draw(nx_graph, pos=nx.spring_layout(nx_graph), node_size=20)
         plt.show()
@@ -50,6 +69,14 @@ class NetworkGenerator(object):
         self.num_nodes = nodes
 
     def ba_network(self, m0=3):
+        """
+        Method for generating Barabassi Albert network,
+        first m0 random nodes are initialized, next other zero degree nodes
+        are connected to other nodes using preferential attachment.
+
+        :param m0: How many nodes new node should choose to associate connection,
+        :return: Network object, containing information about network (adjacency, type)
+        """
         ba_net = np.zeros((self.num_nodes, self.num_nodes))
 
         # Initialize graph
@@ -74,6 +101,15 @@ class NetworkGenerator(object):
         return Network(ba_net, n_type="BA")
 
     def bb_network(self, m0=3, fitness=None):
+        """
+        Method for generating Barabassi Bianconi network, this model besides of
+        preferential attachment and growth as BA model have also parameter called
+        fitness, this fact facilitate younger nodes to overrun older ones.
+        :param m0:          How many nodes new node should choose to associate connection,
+        :param fitness:     Fitness parameter gives information about node importance, if
+                            None will be set at random.
+        :return: Network object, containing information about network (adjacency, type)
+        """
         bb_net = np.zeros((self.num_nodes, self.num_nodes))
 
         # Initialize fitness if None
@@ -103,6 +139,13 @@ class NetworkGenerator(object):
         return Network(bb_net, n_type="BB")
 
     def er_network(self, p=0.5):
+        """
+        Method for generating Erdos-Renyi network. ER network is other name for random graph.
+        First N n connected points are generated, next each edge is connected or not with
+        probability p.
+        :param p:   Random network probability, connect or not,
+        :return: Network object, containing information about network (adjacency, type)
+        """
         er_net = np.random.uniform(0, 1, size=(self.num_nodes, self.num_nodes))
         np.fill_diagonal(er_net, 0)
         er_net *= np.tri(*er_net.shape)
