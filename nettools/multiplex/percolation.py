2782d65299b18f5ba4742f0d87eda46f70214dd6
filip141 <filip141@gmail.com>
2017-04-21 00:07:54 +0200

    ipynb v4

--- nettools/multiplex/percolation.py
+++ nettools/multiplex/percolation.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+import networkx as nx
 import matplotlib.pyplot as plt
 from nettools.multiplex import MultiplexNetwork
 
@@ -27,7 +28,7 @@ class Percolation(object):
     def remove_nodes(self, probability):
         new_net = MultiplexNetwork(self.network.network.copy())
         per_prob = np.random.uniform(0, 1, size=(self.network.get_nodes_num(),))
-        for rmnode in np.where(per_prob < (1 - probability))[0]:
+        for rmnode in np.where(per_prob < probability)[0]:
             new_net.remove_node(rmnode)
         while True:
             rmdeg = np.sum(new_net.network, axis=1)
@@ -40,7 +41,7 @@ class Percolation(object):
                 new_net.remove_node(rmnode)
         return len(new_net.giant_connected_component())
 
-    def run(self, visualize=False, npoints=100, log=False):
+    def run(self, visualize=False, npoints=100, log=False, smooth=True, colour='b'):
         gcc_nodes = []
         for proba_nmul in range(0, npoints):
             probability = proba_nmul / float(npoints)
@@ -51,29 +52,43 @@ class Percolation(object):
         nodes_num = float(self.network.network.shape[0])
         gcc_nodes = np.array(gcc_nodes) / nodes_num
         x_p = np.arange(0, npoints) / float(npoints)
-        x_p = x_p * np.mean(np.sum(self.network.network, axis=1))
         # Visualize
         if visualize:
-            plt.plot(x_p, gcc_nodes)
-            plt.hold(True)
-            plt.plot(x_p[19:], running_mean(gcc_nodes, 20), 'r')
-            plt.show()
+            ax = plt.gca()
+            ax.set_xlim([0, 1.0])
+            ax.set_ylim([0, 1.0])
+            if not smooth:
+                plt.plot(x_p, gcc_nodes, hold=True)
+            plt.plot(x_p[19:], running_mean(gcc_nodes, 20), colour, hold=True)
         return x_p, gcc_nodes
 
 
 if __name__ == '__main__':
     from nettools.monoplex import NetworkGenerator, Network
     from nettools.multiplex import MultiplexConstructor
-    nodes_nm = 100
+    nodes_nm = 1200
     ng = NetworkGenerator(nodes=nodes_nm)
     # ba1 = ng.bb_network(m0=1)
-    # ba2 = ng.bb_network(m0=1)
-    er1 = ng.er_network(p=2.5 / float(nodes_nm))
-    er2 = ng.er_network(p=2.5 / float(nodes_nm))
+    ba1 = ng.bb_network(m0=2)
+    # ba1 = Network(nx.to_numpy_matrix(nx.barabasi_albert_graph(nodes_nm, m=2)), n_type="BA")
+    # er1 = ng.er_network(p=4.0 / float(nodes_nm))
+    # er1 = Network(nx.to_numpy_matrix(nx.erdos_renyi_graph(nodes_nm, p=4.0 / float(nodes_nm - 1))), n_type='ER')
+    er1 = ng.er_network(p=4.0 / float(nodes_nm - 1))
     mc = MultiplexConstructor()
-    # bac = mc.rewire_hubs(ba1, rsteps=2000)
-    mnet = mc.construct(er1, er2)
-    per = Percolation(mnet)
-    per.run(visualize=True, npoints=3000, log=True)
+    # er1 = ng.er_network(p=4.0 / float(nodes_nm))
+    # ba1 = ng.ba_network(m0=2)
+    # er1 = Network(nx.to_numpy_matrix(nx.erdos_renyi_graph(nodes_nm, p=4.0 / float(nodes_nm))), n_type="ER")
+    # ba1 = Network(nx.to_numpy_matrix(nx.barabasi_albert_graph(nodes_nm, m=2)), n_type="BA")
+    # bb1 = ng.bb_network(m0=2)
+    mnet_er = mc.construct(er1)
+    mnet_ba = mc.construct(ba1)
+    # mnet_bb = mc.construct(bb1)
+    per_er = Percolation(mnet_er)
+    per_ba = Percolation(mnet_ba)
+    # per_bb = Percolation(mnet_bb)
+    per_er.run(visualize=True, npoints=1000, colour='blue', log=True)
+    per_ba.run(visualize=True, npoints=1000, colour='red', log=True)
+    # res_bb = per_bb.run(visualize=True, npoints=3000, colour='orange')
+    plt.show()
 
 
